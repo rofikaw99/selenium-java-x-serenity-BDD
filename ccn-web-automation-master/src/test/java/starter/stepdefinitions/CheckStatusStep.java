23bfe919c5c9d901d5918d84edf008846b274204
Aisyah Nurulhaqi Syahidah <aisyah.nurulhaqi.syahidah@gmail.com>
2024-07-18 20:40:17 +0700

    update check status

--- ccn-web-automation-master/src/test/java/starter/stepdefinitions/CheckStatusStep.java
+++ ccn-web-automation-master/src/test/java/starter/stepdefinitions/CheckStatusStep.java
@@ -14,7 +14,7 @@ public class CheckStatusStep {
     @And("user get service id {string} with reference ID")
     public void userGetServiceIdWithReferenceID(String type) {
         if (type.equals("Service")) checkStatusPage.getCheckStatus("5c3d42c5-d14a-4ba4-a3d7-77cf68e0bd99");
-        else checkStatusPage.getCheckStatus("4e59bb35-fc37-48d7-9852-a25a6e0b9190");
+        else checkStatusPage.getCheckStatus("e29da7aa-92cf-450e-87a1-ee914a94da5f");
     }
 
     @Given("user get SSO token for check status")
@@ -25,5 +25,10 @@ public class CheckStatusStep {
     @Then("success get detail of service id")
     public void successGetDetailOfServiceId() {
         checkStatusPage.assertSuccessCheckStatus();
+    }
+
+    @Then("failed with {int} get detail of service id")
+    public void failedWithGetDetailOfServiceId(int statusCode) {
+        checkStatusPage.assertFailedCheckStatus(statusCode);
     }
 }
